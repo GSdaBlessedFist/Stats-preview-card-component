83feef4cfb04539ea537f5a42ebb4b9868d8ffde
GSdaBlessedFist <gsdablessedfist@gmail.com>
2021-07-28 01:33:40 -0500

    added keyframes for minor pulse effect

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,54 @@
 import "../SCSS/styles.scss";
-
-export default function Card(){
-
-	var imageStyle = {
-		border: '0px white solid',
-		textDecoration:'none'
-	}
-	return (<>
-		<div className="card">
-			 <img className="card__image-hero" style={imageStyle} alt=""/>
-			 <h1 className="card__text1"><b>Get <span className="accent">insights</span> that help your business grow.</b></h1>
-			 <div className="card__text2">
-			 	Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
-			 </div>
-			 <div className="card__info-section">
-			 	<div>
-			 		<h2>10k+</h2>
-			 		<h5>COMPANIES</h5>
-			 	</div>
-			 	<div>
-			 		<h2>314</h2>
-			 		<h5>TEMPLATES</h5>
-			 	</div>
-			 	<div>
-			 		<h2>12M+</h2>
-			 		<h5>QUERIES</h5>
-			 	</div>
-			 </div>
-
-		</div>
-	</>);
+import { useEffect} from "react";
+export default function Card() {
+    useEffect(() => {
+            var h2Group = document.querySelectorAll('h2');
+            var count = 750;
+            h2Group.forEach((h2) => {
+                setTimeout(() => {
+                    h2.classList.add('pulse');
+                }, count)
+                count += 750;
+            })
+        
+    }, []);
+var imageStyle = {
+    border: '0px white solid',
+    textDecoration: 'none'
+}
+return ( < >
+    <
+    div className = "card" >
+    <
+    img className = "card__image-hero"
+    style = {
+        imageStyle
+    }
+    alt = "" / >
+    <
+    h1 className = "card__text1" > < b > Get < span className = "accent" > insights < /span> that help your business grow.</b > < /h1> <
+    div className = "card__text2" >
+    Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. <
+    /div> <
+    div className = "card__info-section" >
+    <
+    div >
+    <
+    h2 > 10 k + < /h2> <
+    h5 > COMPANIES < /h5> <
+    /div> <
+    div >
+    <
+    h2 > 314 < /h2> <
+    h5 > TEMPLATES < /h5> <
+    /div> <
+    div >
+    <
+    h2 > 12 M + < /h2> <
+    h5 > QUERIES < /h5> <
+    /div> <
+    /div>
+    <
+    /div> <
+    />);
 }
